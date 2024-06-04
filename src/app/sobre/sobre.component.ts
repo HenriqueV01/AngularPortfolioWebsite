2dc7e45fa8798ae09debb049b83d091be75a032f
HenriqueV01 <henrique.unibratec01@gmail.com>
2024-06-04 17:14:49 -0300

    Fix: curriculo

--- src/app/sobre/sobre.component.ts
+++ src/app/sobre/sobre.component.ts
@@ -16,7 +16,7 @@ export class SobreComponent {
     const link = this.renderer.createElement('a');
     link.setAttribute('target','_blank');
     link.setAttribute('href','../../assets/Curriculo-Henrique-Venancio.pdf');
-    link.setAttribute('download','Sobre.pdf');
+    link.setAttribute('download','Curriculo-Henrique-Venancio.pdf');
     link.click();
     link.remove();
   }
